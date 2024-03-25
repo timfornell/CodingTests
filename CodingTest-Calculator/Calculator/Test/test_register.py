cedd8df328fb2a73c8751e2156c094f54495c6b0
Vincent <tim.fornell@hotmail.com>
2024-03-25 20:06:04 +0100

    Add Test/test_register.py

--- CodingTest-Calculator/Calculator/Test/test_register.py
+++ CodingTest-Calculator/Calculator/Test/test_register.py
@@ -6,7 +6,7 @@ from Operators import Add
 
 def test_initialization():
    name = "TestName"
-   register = Register()
+   register = Register(name)
 
    assert register.name == name
    assert register.current_value == 0.0
@@ -16,6 +16,49 @@ def test_initialization():
 
 def test_operation_storage():
    name = "TestName"
-   register = Register()
+   register = Register(name)
 
-   register.Add
+   operation = Add()
+   value = "200"
+
+   register.AddOperation(operation, value)
+
+   stored_operations = register.GetStoredOperations()
+
+   assert len(stored_operations) == 1
+   assert stored_operations[0]["operation"] == operation
+   assert stored_operations[0]["value"] == value
+
+
+def test_operation_clear():
+   name = "TestName"
+   register = Register(name)
+
+   operation = Add()
+   value = "200"
+
+   register.AddOperation(operation, value)
+
+   stored_operations = register.GetStoredOperations()
+
+   assert len(stored_operations) == 1
+   assert stored_operations[0]["operation"] == operation
+   assert stored_operations[0]["value"] == value
+
+   register.ClearStoredOperations()
+
+   stored_operations = register.GetStoredOperations()
+
+   assert len(stored_operations) == 0
+
+
+def test_set_and_get_value():
+   name = "TestName"
+   register = Register(name)
+
+   value = 200.0
+   register.SetCurrentValue(value)
+   register.InitializeRegister()
+
+   assert register.IsInitialized()
+   assert register.GetCurrentValue() == value # float comparison is a bit risky...
